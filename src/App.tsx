6752ebff1a1bae6475f47b445395a2e0cebec8e0
smiley8608 <tsakthibala@gmail.com>
2022-11-25 18:52:19 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,65 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   
   Autocomplete,
+
   TextField,
   Typography,
 } from "@mui/material";
 import axios from "axios";
 import { Box } from "@mui/system";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridColumns } from "@mui/x-data-grid";
 
 function App() {
   
   const [data, setData] = useState<any>([]);
-  const [value, setValue] = useState<any>([]);
-  const column=useMemo(
-    ()=>[
-      {field:'id',headerName:"S.no" ,width:160},
-      {field:'first_name',headerName:'FirstName',width:160},
-      {field:'last_name',headerName:'LastName',width:260},
-      {field:'email',headerName:'Email',width:360},
-      {field:'gender',headerName:'Gender',width:260},
-    ],[]
-  );
+  const [values, setValues] = useState<any>([]);
+  const [letters, setLetters] = useState<any>("");
+  const column:GridColumns=[
+      {field:'id',headerName:"S.no" ,width:160, editable:true},
+      {field:'first_name',headerName:'FirstName',width:160,editable:true},
+      {field:'last_name',headerName:'LastName',width:260,editable:true},
+      {field:'email',headerName:'Email',width:360,editable:true},
+      {field:'gender',headerName:'Gender',width:260,editable:true},
+    ]
+
   useEffect(() => {
     axios
       .get("http://localhost:3004/getalldata")
       .then((responce) => {
         console.log(responce.data);
         setData(responce.data.User);
+        setValues(responce.data.User);
       })
       .catch();
   }, [setData]);
-  const submitHandler = (id: any) => {
-    console.log(id);
+  const submitHandler = (e:any,value:any) => {
+    console.log(value);
+    const result =value.split(['-'])
+    console.log(result);
+    axios.post('http://localhost:3004/searchuser',{result})
+    .then(responce=>{
+      console.log(responce.data);
+      setData(responce.data.User)
+    })
+    .catch()
+
+    
   };
+  const changehandler=(e:any)=>{
+    console.log(e.target.value);
+     setLetters(e.target.value)
+    console.log(letters);
+     axios.post('http://localhost:3004/searchbyletter',{letters})
+     .then(responce=>{
+      console.log(responce.data);
+      
+     })
+    
+  }
+  const clickhandler=(e:any)=>{
+    console.log(e.row);
+  }
   return (
     <div>
       
@@ -43,7 +69,9 @@ function App() {
         </Typography>
         <Autocomplete      
         disableClearable
-        options={data.map((list:any) => list.first_name +'-'+list.last_name )}
+        options={values.map((list:any)=>list.first_name +'-'+ list.last_name)}
+        
+        onChange={submitHandler}
         
         // value={}
         // onChange={(e: any, newValue: string | null) => {
@@ -57,15 +85,19 @@ function App() {
               ...params.InputProps,
               type: 'search',
             }}
+            onChange={changehandler}
           />
         )}
       />
       <DataGrid 
       columns={column}
       rows={data}
+      experimentalFeatures={{ newEditingApi: true }}
       pageSize={10}
       rowsPerPageOptions={[10]}
-      checkboxSelection
+      
+      onCellClick={clickhandler}
+      
       />
       </Box>
 
